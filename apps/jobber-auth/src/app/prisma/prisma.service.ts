9c0cc98fe0739deab0194f0ebab7d39143d43aad
cuoda <cuoda.me@gmail.com>
2026-09-01 17:57:01 +0530

    implementing authentication

--- apps/jobber-auth/src/app/prisma/prisma.service.ts
+++ apps/jobber-auth/src/app/prisma/prisma.service.ts
@@ -1,7 +1,17 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import {PrismaClient} from '@prisma/client/jobber-auth/client.js'
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../../generated/prisma/client';
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
